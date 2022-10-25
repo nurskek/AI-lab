6cf7238231f12a7ce0d80acf4ca9b1389c073451
Nursultan Kobeisinov <nurs@Nursultans-MacBook-Air.local>
2022-10-25 17:37:04 +0900

    firestore added and readme modified

--- 2022-10-21-myProject/form_app/README.md
+++ 2022-10-21-myProject/form_app/README.md
@@ -3,7 +3,9 @@
 App with login option (gmail login is in process) to submit form
 
 ## References
-
+* https://firebase.flutter.dev/docs/firestore/usage
+* https://firebase.flutter.dev/docs/auth/manage-users
+* https://firebase.flutter.dev/docs/auth/social -> failed to implement
 ## Compatibilty Test
 
 | Android | iPhone (7)    | Windows | macOS (12.6) | Linux | Chrome | Firefox | Safari | Edge | Mobile Chrome on Android)
--- 2022-10-21-myProject/form_app/lib/screens/home_screen.dart
+++ 2022-10-21-myProject/form_app/lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:form_app/screens/signin_screen.dart';
+
+// import 'package:cloud_functions/cloud_functions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,25 +13,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController sampleData1 =  TextEditingController();
+  TextEditingController sampleData2 =  TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  child: const Text("Logout"),
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut().then((value) => {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignInScreen()))
-                        });
-                  },
-                )
-              ]),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              controller: sampleData1,
+              decoration: const InputDecoration(hintText: "Restaurant Name"),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: sampleData2,
+              decoration: const InputDecoration(hintText: "How good was it?"),
+            ),
+            const SizedBox(height: 10),
+            TextButton(onPressed: () {
+              Map<String, dynamic> data = {"resto_name": sampleData1.text, "grade": sampleData2.text};
+              FirebaseFirestore.instance.collection("reviews").add(data);
+            }, child: Text("Submit")),
+            ElevatedButton(
+              child: const Text("Logout"),
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((value) => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignInScreen()))
+                    });
+              },
+            )
+          ]),
     ));
   }
 }
